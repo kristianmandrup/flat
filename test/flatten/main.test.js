6ca12188a095cbc02b4d1638d6214f8a2907a09c
Kristian Mandrup <kmandrup@gmail.com>
2016-11-05 01:00:11 +0100

    getting close :)

--- test/flatten/main.test.js
+++ test/flatten/main.test.js
@@ -3,50 +3,6 @@ const { flatten, unflatten } = require('../../index')
 const flat = flatten
 
 suite('Flatten', function() {
-  test('Nested once', function() {
-    assert.deepEqual(flatten({
-      hello: {
-        world: 'good morning'
-      }
-    }), {
-      'hello.world': 'good morning'
-    })
-  })
-
-  test('Nested twice', function() {
-    assert.deepEqual(flatten({
-      hello: {
-        world: {
-          again: 'good morning'
-        }
-      }
-    }), {
-      'hello.world.again': 'good morning'
-    })
-  })
-
-  test('Multiple Keys', function() {
-    assert.deepEqual(flatten({
-      hello: {
-        lorem: {
-          ipsum: 'again',
-          dolor: 'sit'
-        }
-      },
-      world: {
-        lorem: {
-          ipsum: 'again',
-          dolor: 'sit'
-        }
-      }
-    }), {
-      'hello.lorem.ipsum': 'again',
-      'hello.lorem.dolor': 'sit',
-      'world.lorem.ipsum': 'again',
-      'world.lorem.dolor': 'sit'
-    })
-  })
-
   test('Custom Delimiter', function() {
     assert.deepEqual(flatten({
       hello: {
